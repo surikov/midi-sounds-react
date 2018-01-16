dbb884db2865c2e1ad5aad3bfbb8540fb78c28e4
Srgy Surkv <sss1024@gmail.com>
2018-01-16 18:24:33 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MIDISounds from './MIDISounds.jsx';
-const sample={i:null};
+const sample = { i: null };
 class App extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		console.log('App constructor');
-		this.state = {value: 2};
+		this.state = { value: 2 };
 		//this.instrumentItems=[];
 		/*console.log(this);
 		console.log(this.midiSounds);
@@ -18,49 +18,61 @@ class App extends Component {
 		}*/
 		console.log('done App constructor');
 	}
-	clickLog(){
+	clickLog() {
 		console.log('clickLog');
 		this.midiSounds.logStatus();
-  }
-  componentWillReceiveProps(nextProps: IWorkItemTypeDropdownProps, nextContext: any) {
-        this.setState({
-            value: ""
-        });
-    }
-  clickProp(){
+	}
+	componentWillReceiveProps(nextProps: IWorkItemTypeDropdownProps, nextContext: any) {
+		this.setState({
+			value: ""
+		});
+	}
+	clickProp() {
 		console.log('clickProp');
 		this.midiSounds.showPropertiesDialog();
-  }
-   createSelectItems() {
-	   console.log('createSelectItems',this);
-	   //console.log('createSelectItems.midiSounds',this.midiSounds);
-     let items = []; 
-	 var arr=[];//me.midiSounds.player.loader.instrumentKeys();;
-	 //['sdfv','zdfb','zdfb','aer','w5g'];
-	 if(this.state){
-	 if(this.state.instruments){arr=this.state.instruments;}}
-     for (let i = 0; i < arr.length; i++) {             
-          items.push(<option key={i} value={i}>{arr[i]}</option>);   
-          //here I will be creating my options dynamically based on
-          //what props are currently passed to the parent component
-     }
-     return items;
 	}
-	readInstruments(names) {
-		console.log('readInstruments',names);
-		this.setState({instruments:names});
+	createSelectItems() {
+		console.log('createSelectItems', this);
+		//console.log('createSelectItems.midiSounds',this.midiSounds);
+		let items = [];
+		//var arr = [];//me.midiSounds.player.loader.instrumentKeys();;
+		//['sdfv','zdfb','zdfb','aer','w5g'];
+		//if (this.state) {
+		//if (this.state.instruments) { arr = this.state.instruments; }
+		//}
+		if (this.midiSounds) {
+			var arr=[];
+			for (var i = 0; i < this.midiSounds.player.loader.instrumentKeys().length; i++) {
+				arr.push('' + (i + 0) + '. ' + this.midiSounds.player.loader.instrumentInfo(i).title);
+			  }
+			for (let i = 0; i < arr.length; i++) {
+				items.push(<option key={i} value={i}>{arr[i]}</option>);
+				//here I will be creating my options dynamically based on
+				//what props are currently passed to the parent component
+			}
+		}
+		return items;
 	}
-	selectIns(e){//: React.FormEvent){
-		console.log('selectIns',this,e);
+	componentDidMount() {
+		console.log('componentDidMount App');
+		this.setState({ initialized: true });
+	}
+	readInstruments() {
+		console.log('readInstruments');
+		this.setState({ initialized: true });
+	}
+	selectIns(e) {//: React.FormEvent){
+		console.log('selectIns', this, e);
 		let select = e.target;// as HTMLSelectElement;
-        let n = select.options[select.selectedIndex].getAttribute("value");
+		let n = select.options[select.selectedIndex].getAttribute("value");
 
-        this.setState({
-            value: n
-        });
-console.log('select',n);
-        //this.props.workItemTypeChanged(e);
-		var m=this.midiSounds;
+		this.setState({
+			value: n
+		});
+		console.log('select', n);
+		this.midiSounds.cacheInstrument(n);
+		//this.props.workItemTypeChanged(e);
+		/*var m=this.midiSounds;
 		var info=m.player.loader.instrumentInfo(n)
 				console.log('selectins',n,info);
 				
@@ -74,40 +86,43 @@ console.log('select',n);
 					//started=false;
 					//start();
 					console.log('done',window[info.variable]);
-				});
+				});*/
 	}
-	playCur(){
-		this.midiSounds.player.queueWaveTable(this.midiSounds.audioContext, this.midiSounds.audioContext.destination, sample.i, 0, 55, 3.5);
+	playCur() {
+		//this.midiSounds.player.queueWaveTable(this.midiSounds.audioContext, this.midiSounds.audioContext.destination, sample.i, 0, 55, 3.5);
+		this.midiSounds.playChordNow(this.state.value, [55], 2.5);
+		//this.midiSounds.setInstrumentVolume(this.state.value,0.25);
+		//console.log(this.state.value);
 	}
-  render() {
-	  console.log('App render');
-    var r= (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to MIDI Sounds React</h1>
-        </header>
-		
-		<h2>Component</h2>
-		<MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" sendInstruments={this.readInstruments.bind(this)} />
-		
-		
-		
-        <h2>Simple sound</h2>
-		<select value={this.state.value} onChange={this.selectIns.bind(this)}>{this.createSelectItems()}</select>
-        <p><a href='#' onClick={this.playCur.bind(this)}>Play</a></p>
-		
-		
-		
-		
-		
-		
-		<p>Info <button type="button" onClick={this.clickLog.bind(this)} >Log status</button></p>
-      </div>
-    );
-	console.log('done App render');
-	return r;
-  }
+	render() {
+		console.log('App render');
+		var r = (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to MIDI Sounds React</h1>
+				</header>
+
+				<h2>Component</h2>
+				<MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" afterInit222={this.readInstruments.bind(this)} instruments={[4, 44, 444]} drums={[2, 22, 222]} />
+
+
+
+				<h2>Simple sound</h2>
+				<select value={this.state.value} onChange={this.selectIns.bind(this)}>{this.createSelectItems()}</select>
+				<p><a href='#' onClick={this.playCur.bind(this)}>Play</a></p>
+
+
+
+
+
+
+				<p>Info <button type="button" onClick={this.clickLog.bind(this)} >Log status</button></p>
+			</div>
+		);
+		console.log('done App render');
+		return r;
+	}
 }
 
 export default App;
--- src/MIDISounds.jsx
+++ src/MIDISounds.jsx
@@ -4,70 +4,75 @@ import WebAudioFontPlayer from 'webaudiofont';
 
 const STYLE = {
   MIDISoundsInfo: {
-    height:'100%'
-    ,overflow: 'auto'
-    ,paddingRight:'3px'
+    height: '100%'
+    , overflow: 'auto'
+    , paddingRight: '3px'
   }
-  ,MIDISoundsClose: {
+  , MIDISoundsClose: {
     textAlign: 'right'
-    ,borderTop: '1px solid silver'
+    , borderTop: '1px solid silver'
   }
-  ,MIDISoundsClose2: {
+  , MIDISoundsClose2: {
     textAlign: 'right'
-    ,borderBottom: '1px solid silver'
+    , borderBottom: '1px solid silver'
   }
-  ,MIDISoundsEq: {
+  , MIDISoundsEq: {
     writingMode: 'bt-lr' /* IE */
-    ,WebkitAppearance: 'slider-vertical' /* WebKit */
-    ,width: '0.5cm'
-    ,height: '4cm'
-    ,padding: '0 5px'
+    , WebkitAppearance: 'slider-vertical' /* WebKit */
+    , width: '0.5cm'
+    , height: '4cm'
+    , padding: '0 5px'
   }
-  ,MIDISoundsVl: {
+  , MIDISoundsVl: {
     width: '95%'
   }
 };
 class MIDISounds extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    console.log('constructor MIDISounds',props);
+    console.log('constructor MIDISounds', props);
     this.state = {
       showModal: false
-      ,appElementName:this.props.appElementName
-	  ,sendInstruments:this.props.sendInstruments
+      , appElementName: this.props.appElementName
+     // , afterInit: this.props.afterInit
+      , instruments: this.props.instruments
+      , drums: this.props.drums
+      ,master:1.0
     };
-    if(this.props.appElementName){
-      ReactModal.setAppElement('#'+this.props.appElementName);
+    if (this.props.appElementName) {
+      ReactModal.setAppElement('#' + this.props.appElementName);
     }
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
-	this.initAudio();
-	console.log('done constructor MIDISounds',props);
+    this.initAudio();
+    console.log('done constructor MIDISounds', props);
   }
   componentDidMount() {
-	  console.log('componentDidMount MIDISounds');
-	  if(this.props.sendInstruments){
-		//this.props.sendInstruments(['qqq','wwww','ddd']);
-		var arr=[];
-		for(var i = 0; i < this.player.loader.instrumentKeys().length; i++) {
-				arr.push(''+(i+1)+'. '+this.player.loader.instrumentInfo(i).title);
-			}
-		this.props.sendInstruments(arr);
-		}
+    console.log('componentDidMount MIDISounds');
+    //if (this.props.afterInit) {
+      //this.props.sendInstruments(['qqq','wwww','ddd']);
+      /*var arr = [];
+      for (var i = 0; i < this.player.loader.instrumentKeys().length; i++) {
+        arr.push('' + (i + 0) + '. ' + this.player.loader.instrumentInfo(i).title);
+      }
+      this.props.sendInstruments(arr);*/
+      //this.props.afterInit();
+    //}
   }
   componentWillUnmount() {
-	  console.log('componentWillUnmount MIDISounds');
+    console.log('componentWillUnmount MIDISounds');
   }
-  getInitialState() { 
-	console.log('getInitialState MIDISounds');
-    return { 
-        instruments: []
-		,drums:[]
-    }; 
-  }
+  /*getInitialState() {
+    console.log('getInitialState MIDISounds');
+    return {
+      instruments: []
+      , drums: []
+    };
+  }*/
   render() {
     console.log('MIDISounds render');
-    var r= (
+    this.refreshCache();
+    var r = (
       <div className="MIDISounds">
         <button className="MIDISounds" onClick={this.handleOpenModal}>M♩D♩Sounds</button>
         <ReactModal isOpen={this.state.showModal} contentLabel="Minimal Modal Example" >
@@ -75,28 +80,46 @@ class MIDISounds extends React.Component {
             <p style={STYLE.MIDISoundsClose2}><a href='#' onClick={this.resetAudio.bind(this)}>Reset</a> | <a href='https://surikov.github.io/midi-sounds-react/'>Help</a> | <a href='#' onClick={this.handleCloseModal}>Close</a></p>
             <p>Equalizer <a href='#' onClick={this.handleCloseModal}>Power</a> | <a href='#' onClick={this.handleCloseModal}>Dance</a> | <a href='#' onClick={this.handleCloseModal}>None</a></p>
             <p>
-				<nobr>
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				  <input type='range' style={STYLE.MIDISoundsEq} />
-				</nobr>
+              <nobr>
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+                <input type='range' style={STYLE.MIDISoundsEq} />
+              </nobr>
             </p>
-            <p>Master volume <br/><input type='range' style={STYLE.MIDISoundsVl} /></p>
-            <p>Echo level <br/><input type='range' style={STYLE.MIDISoundsVl} /></p>
+            <p>Master volume <br /><input type='range' value={this.state.master} min={0.0} max={1.5} step={0.1}  style={STYLE.MIDISoundsVl} onChange={this.onChangeMaster.bind(this)}/></p>
+            <p>Echo level <br /><input type='range' style={STYLE.MIDISoundsVl} /></p>
             <p>MIDI input: initializing</p>
           </div>
         </ReactModal>
       </div>
     );
-	console.log('done MIDISounds render');
-	return r;
+    console.log('done MIDISounds render');
+    return r;
+  }
+  onChangeMaster(e){
+    let n = e.target.value;
+    //console.log('onChangeMaster',n,this);
+  	
+    this.setMasterVolume(n);
+  }
+  refreshCache() {
+    if (this.state.instruments) {
+      for (var i = 0; i < this.state.instruments.length; i++) {
+        this.cacheInstrument(this.state.instruments[i]);
+      }
+    }
+    if (this.state.drums) {
+      for (var i = 0; i < this.state.drums.length; i++) {
+        this.cacheDrum(this.state.drums[i]);
+      }
+    }
   }
   logStatus() {
     console.log('logStatus M♩D♩Sounds');
@@ -105,23 +128,137 @@ class MIDISounds extends React.Component {
     console.log('showPropertiesDialog M♩D♩Sounds');
     this.handleOpenModal();
   }
-  handleOpenModal () {
+  handleOpenModal() {
     this.setState({ showModal: true });
   }
-  
-  handleCloseModal () {
+
+  handleCloseModal() {
     this.setState({ showModal: false });
   }
-  initAudio(){
-	  console.log('initAudio M♩D♩Sounds');
-	  var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
-	  this.audioContext = new AudioContextFunc();
-	  this.target = this.audioContext.destination;
-	  this.player=new WebAudioFontPlayer();
+  initAudio() {
+    console.log('initAudio M♩D♩Sounds');
+    var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+    this.audioContext = new AudioContextFunc();
+    this.target = this.audioContext.destination;
+    this.player = new WebAudioFontPlayer();
+    this.equalizer = this.player.createChannel(this.audioContext);
+    this.echo = this.player.createReverberator(this.audioContext);
+    this.echo.output.connect(this.target);
+    this.equalizer.output.connect(this.echo.input);
+    this.volumesInstrument = [];
+    this.volumesDrums = [];
+    //this.cacheInstrument(685);
+    //this.cacheDrum(67);
   }
-  resetAudio(){
-	  this.initAudio();
-	  this.handleCloseModal();
+  resetAudio() {
+    this.initAudio();
+    this.handleCloseModal();
+  }
+  cacheInstrument(n) {
+    var info = this.player.loader.instrumentInfo(n);
+    if (window[info.variable]) {
+      return;
+    }
+    this.player.loader.startLoad(this.audioContext, info.url, info.variable);
+    this.player.loader.waitLoad(function () {
+      console.log('cached', n, info.title);
+    });
+  }
+  cacheDrum(n) {
+    var info = this.player.loader.drumInfo(n);
+    if (window[info.variable]) {
+      return;
+    }
+    this.player.loader.startLoad(this.audioContext, info.url, info.variable);
+    this.player.loader.waitLoad(function () {
+      console.log('cached', n, info.title);
+    });
+  }
+  playDrumsAt(when, drums) {
+
+  }
+  playChordAt(when, instrument, pitches, duration) {
+    var info = this.player.loader.instrumentInfo(instrument);
+    if(window[info.variable]){
+      var volume=1;
+      if(this.volumesInstrument[instrument]===undefined){
+        //
+      }else{
+        volume=this.volumesInstrument[instrument];
+      }
+      this.player.queueChord(this.audioContext, this.equalizer.input, window[info.variable], when, pitches, duration, volume);
+    }else{
+      this.cacheInstrument(instrument);
+    }
+  }
+  playStrumUpAt(when, instrument, pitches, duration) {
+
+  }
+  playStrumDownAt(when, instrument, pitches, duration) {
+
+  }
+  playSnapAt(when, instrument, pitches, duration) {
+
+  }
+  playDrumsNow(drums) {
+
+  }
+  playChordNow(instrument, pitches, duration) {
+    this.playChordAt(0, instrument, pitches, duration);
+  }
+  playStrumUpNow(instrument, pitches, duration) {
+
+  }
+  playStrumDownNow(instrument, pitches, duration) {
+
+  }
+  playSnapNow(instrument, pitches, duration) {
+
+  }
+  setMasterVolume(volume) {
+    this.setState({
+			master: volume
+    });
+    this.echo.output.gain.setTargetAtTime(volume, 0, 0.0001);
+  }
+  setInstrumentVolume(instrument, volume) {
+    this.volumesInstrument[instrument] = volume;
+  }
+  setDrumVolume(drum, volume) {
+    this.volumesDrum[drum] = volume;
+  }
+  setEchoLevel(value) {
+    this.echo.wet.gain.setTargetAtTime(value, 0, 0.0001);
+  }
+  setBand32(level) {
+    this.equalizer.band32.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand64(level) {
+    this.equalizer.band64.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand128(level) {
+    this.equalizer.band128.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand256(level) {
+    this.equalizer.band256.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand512(level) {
+    this.equalizer.band512.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand1k(level) {
+    this.equalizer.band1k.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand2k(level) {
+    this.equalizer.band2k.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand4k(level) {
+    this.equalizer.band4k.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand8k(level) {
+    this.equalizer.band8k.gain.setTargetAtTime(level, 0, 0.0001);
+  }
+  setBand16k(level) {
+    this.equalizer.band16k.gain.setTargetAtTime(level, 0, 0.0001);
   }
 }
 
