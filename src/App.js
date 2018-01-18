2ffc51b87d4d218d0e210b5c92e866b97a261334
Srgy Surkv <sss1024@gmail.com>
2018-01-18 18:41:18 +0300

    test

--- src/App.js
+++ src/App.js
@@ -36,9 +36,9 @@ const snare=17;
 const pedal=35;
 const bass=437;
 const hit=609;
-const synth=161;//518;//564;//544;
+const synth=521;//161;//518;//564;//544;
 const notes=[
-	[[pedal,drum      ],[[bass,O*2+C,1/16],[hit,O*5+C,1/4],[synth,O*2+C,1/1],[synth,O*4+C,1/1],[synth,O*2+G,1/1],[synth,O*5+C,1/2],[synth,O*5+d,3/8]]]//1/16
+	[[pedal,drum      ],[[bass,O*2+C,1/16],[hit,O*5+C,1/4],[synth,O*3+C,1/1],[synth,O*4+C,1/1],[synth,O*3+G,1/1],[synth,O*5+C,1/2],[synth,O*5+d,3/8]]]//1/16
    ,[[pedal           ],[                                                                                                                           ]]
    ,[[hihat           ],[[bass,O*2+C,1/16]                                                                                                          ]]
    ,[[                ],[[bass,O*2+C,1/16]                                                                                                          ]]
@@ -47,14 +47,14 @@ const notes=[
    ,[[hihat           ],[[bass,O*2+C,1/16],                [synth,O*5+d,1/8]                                                                        ]]
    ,[[                ],[[bass,O*2+C,1/16]                                                                                                          ]]
    ,[[pedal,drum      ],[[bass,O*2+C,1/16],                [synth,O*5+C,1/8]                                                                        ]]//
-   ,[[pedal           ],[[bass,O*2+C,1/16],                [synth,O*2+C,1/1]                                                                        ]]
+   ,[[pedal           ],[[bass,O*2+C,1/16],                [synth,O*3+C,1/1]                                                                        ]]
    ,[[hihat           ],[                                  [synth,O*5+d,1/8]                                                                        ]]
    ,[[                ],[[bass,O*2+C,1/16]                                                                                                          ]]
    ,[[pedal,drum,snare],[[bass,O*2+C,1/16],                [synth,O*5+d,1/8]                                                                        ]]
    ,[[pedal           ],[[bass,O*2+C,1/16]                                                                                                          ]]
    ,[[hihat           ],[[bass,O*2+C,1/16],[hit,O*4+G,1/8],[synth,O*5+G,1/8]                                                                        ]]
    ,[[                ],[[bass,O*2+C,1/16]                                                                                                          ]]
-   ,[[pedal,drum      ],[[bass,O*1+G,1/16],[hit,O*5+a,1/4],[synth,O*2+G,1/1],[synth,O*4+G,1/1],[synth,O*5+d,3/1],[synth,O*5+a,3/8]                  ]]//16/16
+   ,[[pedal,drum      ],[[bass,O*1+G,1/16],[hit,O*5+a,1/4],[synth,O*3+G,1/1],[synth,O*4+G,1/1],[synth,O*5+d,3/1],[synth,O*5+a,3/8]                  ]]//16/16
    ,[[pedal           ],[                                                                                                                           ]]
    ,[[hihat           ],[[bass,O*1+G,1/16]                                                                                                          ]]
    ,[[                ],[[bass,O*1+G,1/16]                                                                                                          ]]
@@ -70,7 +70,7 @@ const notes=[
    ,[[pedal           ],[[bass,O*1+G,1/16]                                                                                                          ]]
    ,[[hihat           ],[[bass,O*1+G,1/16],[hit,O*5+d,1/8],[synth,O*6+D,1/8]                                                                        ]]
    ,[[                ],[[bass,O*1+G,1/16]                                                                                                          ]]
-   ,[[pedal,drum      ],[[bass,O*1+a,1/16],[hit,O*5+F,1/1],[synth,O*2+a,2/1],[synth,O*4+a,2/1],[synth,O*5+F,2/1],[synth,O*6+F,2/1]                  ]]//32/16
+   ,[[pedal,drum      ],[[bass,O*1+a,1/16],[hit,O*5+F,1/1],[synth,O*3+a,2/1],[synth,O*4+a,2/1],[synth,O*5+F,2/1],[synth,O*6+F,2/1]                  ]]//32/16
    ,[[pedal           ],[                                                                                                                           ]]
    ,[[hihat           ],[[bass,O*1+a,1/16]                                                                                                          ]]
    ,[[                ],[[bass,O*1+a,1/16]                                                                                                          ]]
@@ -111,7 +111,14 @@ class App extends Component {
 		this.state = {
 			selInsNum: 260
 			,selDrNum:1
-			, masterVolume: 0.9
+			//, masterVolume: 0.9
+			,volDrum:0.7
+			,volSnare:0.7
+			,volHat:0.3
+			,volPedal:0.8
+			,volBass:1
+			,volHit:0.5
+			,volSynth:0.5
 		};
 	}
 	clickLog() {
@@ -155,6 +162,7 @@ class App extends Component {
 	}
 	componentDidMount() {
 		//console.log('componentDidMount App');
+		this.sendVolumes();
 		this.setState({ initialized: true });
 	}
 	selectIns(e) {
@@ -217,12 +225,53 @@ class App extends Component {
 		this.midiSounds.playDrumsAt(this.midiSounds.audioContext.currentTime + 0.15 * 3,[this.state.selDrNum]);
 		this.midiSounds.playDrumsAt(this.midiSounds.audioContext.currentTime + 0.15 * 4,[this.state.selDrNum]);
 	}
-	changeMasterVolume(e) {
-		let n = e.target.value;
-		this.midiSounds.setMasterVolume(n);
-		this.setState({
-			masterVolume: n
-		});
+	changeMasterQuiet() {
+		this.midiSounds.setMasterVolume(0.3);
+	}
+	changeMasterLoud() {
+		this.midiSounds.setMasterVolume(1);
+	}
+	changeMasterEchoStrong() {
+		this.midiSounds.setEchoLevel(1);
+	}
+	changeMasterEchoNone() {
+		this.midiSounds.setEchoLevel(0);
+	}
+	changeMasterPower() {
+		this.midiSounds.setBand32(2);
+		this.midiSounds.setBand64(4);
+		this.midiSounds.setBand128(3);
+		this.midiSounds.setBand256(-2);
+		this.midiSounds.setBand512(-3);
+		this.midiSounds.setBand1k(1);
+		this.midiSounds.setBand2k(2);
+		this.midiSounds.setBand4k(3);
+		this.midiSounds.setBand8k(-3);
+		this.midiSounds.setBand16k(1);
+	}
+	changeMasterDance() {
+		this.midiSounds.setBand32(2);
+		this.midiSounds.setBand64(2);
+		this.midiSounds.setBand128(1);
+		this.midiSounds.setBand256(-1);
+		this.midiSounds.setBand512(5);
+		this.midiSounds.setBand1k(4);
+		this.midiSounds.setBand2k(4);
+		this.midiSounds.setBand4k(2);
+		this.midiSounds.setBand8k(-2);
+		this.midiSounds.setBand16k(3);
+	}
+	changeMasterNone() {
+		this.midiSounds.setBand32(0);
+		this.midiSounds.setBand64(0);
+		this.midiSounds.setBand128(0);
+		this.midiSounds.setBand256(0);
+		this.midiSounds.setBand512(0);
+		this.midiSounds.setBand1k(0);
+		this.midiSounds.setBand2k(0);
+		this.midiSounds.setBand4k(0);
+		this.midiSounds.setBand8k(0);
+		this.midiSounds.setBand16k(0);
 	}
 	testPly(){
 		//this.midiSounds.setInstrumentVolume(hit,0.1);
@@ -268,9 +317,9 @@ class App extends Component {
 			for(var i=0;i<insArr.length;i++){
 				var noteArr=insArr[i];
 				var num=noteArr[0];
-				if(synth==num){
-					//num=this.state.selInsNum;
-				}
+				/*if(synth==num){
+					num=this.state.selInsNum;
+				}*/
 				this.midiSounds.playChordAt(this.startTime + n * this.beatLen,num,[noteArr[1]],noteArr[2]*this.N);
 				//console.log(noteArr[0],[noteArr[1],noteArr[2]]);
 			}
@@ -291,6 +340,44 @@ class App extends Component {
 		this.setState({masterVolume:this.midiSounds.getProperties().master})
 		
 	}*/
+	onChangeTrackDrum(e){
+		this.setState({volDrum: e.target.value});
+		this.sendVolumes();
+	}
+	onChangeTrackSnare(e){
+		this.setState({volSnare: e.target.value});
+		this.sendVolumes();
+	}
+	onChangeTrackPedal(e){
+		this.setState({volPedal: e.target.value});
+		this.sendVolumes();
+	}
+	onChangeTrackHat(e){
+		this.setState({volHat: e.target.value});
+		this.sendVolumes();
+	}
+	onChangeTrackBass(e){
+		this.setState({volBass: e.target.value});
+		this.sendVolumes();
+	}
+	onChangeTrackHit(e){
+		this.setState({volHit: e.target.value});
+		this.sendVolumes();
+	}
+	onChangeTrackSynth(e){
+		this.setState({volSynth: e.target.value});
+		this.sendVolumes();
+	}
+	sendVolumes(){
+		this.testStp();
+		this.midiSounds.setInstrumentVolume(synth,this.state.volSynth);
+		this.midiSounds.setInstrumentVolume(hit,this.state.volHit);
+		this.midiSounds.setInstrumentVolume(bass,this.state.volBass);
+		this.midiSounds.setDrumVolume(hihat,this.state.volHat);
+		this.midiSounds.setDrumVolume(pedal,this.state.volPedal);
+		this.midiSounds.setDrumVolume(snare,this.state.volSnare);
+		this.midiSounds.setDrumVolume(drum,this.state.volDrum);
+	}
 	render() {
 		//console.log('App render',this.state.masterVolume);
 		var r = (
@@ -321,11 +408,32 @@ class App extends Component {
 				<h2>info</h2>
 				<p>Info <button type="button" onClick={this.clickLog.bind(this)} >Log status</button></p>
 				<h2>master</h2>
-				<p>Master <input type='range' value={this.state.masterVolume} min={0.0} max={1.5} step={0.1} onChange={this.changeMasterVolume.bind(this)} /></p>
+				<p>
+					volume
+					<button type='button' onClick={this.changeMasterQuiet.bind(this)} >Quiet</button>
+					<button type='button' onClick={this.changeMasterLoud.bind(this)} >Loud</button>
+					<br/>echo
+					<button type='button' onClick={this.changeMasterEchoNone.bind(this)} >Off</button>
+					<button type='button' onClick={this.changeMasterEchoStrong.bind(this)} >On</button>
+					<br/>equalizer
+					<button type='button' onClick={this.changeMasterNone.bind(this)} >None</button>
+					<button type='button' onClick={this.changeMasterPower.bind(this)} >Power</button>
+					<button type='button' onClick={this.changeMasterDance.bind(this)} >Dance</button>
+				</p>
 				<h2>tst</h2>
 				<p>
 					<button type="button" onClick={this.testPly.bind(this)} >P</button>
 					<button type="button" onClick={this.testStp.bind(this)} >S</button>
+				</p>
+				<h2>Trks</h2>
+				<p>
+					Drum <input type='range' value={this.state.volDrum} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackDrum.bind(this)} />
+					<br />Snare <input type='range' value={this.state.volSnare} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackSnare.bind(this)} />
+					<br />Pedal <input type='range' value={this.state.volPedal} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackPedal.bind(this)} />
+					<br />HiHat <input type='range' value={this.state.volHat} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackHat.bind(this)} />
+					<br />Bass guitar <input type='range' value={this.state.volBass} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackBass.bind(this)} />
+					<br />Orchestra Hit <input type='range' value={this.state.volHit} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackHit.bind(this)} />
+					<br />Synth strings <input type='range' value={this.state.volSynth} min={0.0} max={1.0} step={0.1} onChange={this.onChangeTrackSynth.bind(this)} />
 				</p>
 			</div>
 		);
