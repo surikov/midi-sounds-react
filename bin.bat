f68818917547ae606289d121971e3b570c06412e
Srgy Surkv <sss1024@gmail.com>
2018-01-22 18:23:56 +0300

    39

--- bin.bat
+++ bin.bat
@@ -1,2 +1,4 @@
-.\node_modules\.bin\babel .\src\midisoundsreact.js > .\bin\midisoundsreact.js
+rem .\node_modules\.bin\babel .\src\midisoundsreact.js > .\bin\midisoundsreact6.js
+echo next
+.\node_modules\.bin\babel .\bin\midisoundsreact6.js --presets es2015 > .\bin\midisoundsreact.js
 pause
--- src/midisoundsreact.js
+++ src/midisoundsreact.js
@@ -57,7 +57,7 @@ const STYLE = {
 class MIDISounds extends React.Component {
   constructor(props) {
     super(props);
-	console.log('MIDISounds v1.2.38');
+	console.log('MIDISounds v1.2.39');
     this.state = {
       showModal: false
       , appElementName: this.props.appElementName
